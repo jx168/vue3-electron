94950a7f1e322b7c0b4fdf387473944a1a270dcf
luo168 <luo168.com>
2024-05-02 04:17:51 +0800

    集成vuetify框架

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import App from './App.vue'
 import router from './router'
 import i18n from '@/locales/i18n'
 import '@/styles/common.less'
+import vuetify from '@/vuetify/vuetifyConfig'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
-app.use(i18n)
+.use(router)
+.use(i18n)
+.use(vuetify)
 
 app.mount('#app')
